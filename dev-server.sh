e7ddadf7e4758fbf3312936b539f81d24ff02421
Daniel Otuo-Acheampong <danielotuoach@gmail.com>
2023-10-27 21:17:46 -0400

    set the env for flask
    set the env for flask

---
+++ dev-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FLASK_APP="store:create_app"
+export FLASK_ENV=development
+flask run
+
---
+++ store/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from store.config import configurations
+
+def create_app(environment_name ="dev"):
+    app = Flask(__name__)
+    app.config.from_object(configurations[environment_name])
+    return app
+
+
+
+
