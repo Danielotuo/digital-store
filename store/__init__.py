a536ddf54c8d8358f34106388fb920ce0423a807
Daniel Otuo-Acheampong <danielotuoach@gmail.com>
2023-10-27 21:36:40 -0400

    Set up SQLAlchemy extensions and productds model

--- store/__init__.py
+++ store/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from store.config import configurations
+from store.extensions import db
 
-def create_app(environment_name ="dev"):
+def create_app(environment_name='dev'):
     app = Flask(__name__)
     app.config.from_object(configurations[environment_name])
+    db.init_app(app)
     return app
-
-
 
 
